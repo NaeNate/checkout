a6c164798a71cdc09e2510eefea7363f6e4672c8
Nate <nate@qstreet.org>
2020-10-03 22:25:55 -0700

    Setup Webpack and Bable with it

---
+++ src/utils.js
@@ -0,0 +1,13 @@
+// console.log('utils running')
+
+// export const square = (x) => x * x
+
+// export const add = (a, b) => a + b
+
+// export default (a, b) => a - b
+
+// export const isAdult = (age) => age > 17
+
+// export const canDrink = (age) => age > 20
+
+export default (age) => age > 64
