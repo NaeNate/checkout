8f7db80d3371aa746828081e6df69aea5d32d111
Nate <nate@qstreet.org>
2020-10-07 22:26:36 -0700

    Setup React-Modal

--- src/app.js
+++ src/app.js
@@ -2,4 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom";
 import IndecisonApp from "./components/IndecisionApp";
 
-ReactDOM.render(<IndecisonApp />, document.getElementById("app"));
+ReactDOM.render(<IndecisonApp />, document.getElementById("app"))
--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -3,10 +3,12 @@ import AddOption from "./AddOption";
 import Header from "./Header";
 import Action from "./Action";
 import Options from "./Options";
+import OptionModal from "./OptionModal";
 
 export default class IndecisonApp extends React.Component {
   state = {
     options: [],
+    selectedOption: undefined,
   };
   handleDeleteOptions = () => {
     this.setState(() => ({ options: [] }));
@@ -19,7 +21,7 @@ export default class IndecisonApp extends React.Component {
   handlePick = () => {
     const randomNum = Math.floor(Math.random() * this.state.options.length);
     const option = this.state.options[randomNum];
-    alert(option);
+    this.setState(() => ({ selectedOption: option }));
   };
   handleAddOption = (option) => {
     if (!option) {
@@ -31,6 +33,9 @@ export default class IndecisonApp extends React.Component {
     this.setState((prevState) => ({
       options: prevState.options.concat(option),
     }));
+  };
+  handleClearOption = () => {
+    this.setState(() => ({ selectedOption: undefined }));
   };
   componentDidMount() {
     try {
@@ -70,6 +75,10 @@ export default class IndecisonApp extends React.Component {
           handleDeleteOption={this.handleDeleteOption}
         />
         <AddOption handleAddOption={this.handleAddOption} />
+        <OptionModal
+          selectedOption={this.state.selectedOption}
+          handleClearOption={this.handleClearOption}
+        />
       </div>
     );
   }
